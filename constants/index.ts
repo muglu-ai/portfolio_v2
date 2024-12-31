e6b76687d8bebf5e528a5d4db144648740d60e0c
Manish Kumar Sharma <manishk_sharma@outlook.com>
2024-12-31 23:07:21 +0530

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,176 @@
+export const Skill_data = [
+    //python
+    {
+      skill_name: "Python",
+      Image: "/python-g.gif",
+      width: 70,
+      height: 70,
+    },
+    //typescript
+    {
+      skill_name: "Type Script",
+      Image: "/ts.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Java",
+      Image: "/java-g.gif",
+      width: 70,
+      height: 70,
+    },
+    //php
+    {
+      skill_name: "PHP",
+      Image: "/php.png",
+      width: 70,
+      height: 70,
+    }
+    //java
+
+
+  ];
+
+  export const Socials = [
+    {
+      name: "Linkedin",
+      src: "/linkedin.svg",
+    },
+    {
+      name: "Github",
+      src: "/github.svg",
+    },
+  ];
+
+
+
+  export const Frontend_skill = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "CSS",
+      Image: "/css.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "JavaScript",
+      Image: "/js.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Tailwind CSS",
+      Image: "/tailwind.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/ts.png",
+      width: 70,
+      height: 70,
+    },
+  ];
+
+  export const Backend_skill = [
+    {
+      skill_name: "NextJS",
+      Image: "/next.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Django",
+      Image: "/django.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Laravel",
+      Image: "/laravel.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "PostgerSQL",
+      Image: "/postger.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "MySQL",
+      Image: "/mysql.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "API",
+      Image: "/rest-api-w.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      //payment gateway integration
+        skill_name: "Payment Gateway Integration",
+        Image: "/payment-gateway.png",
+        width: 70,
+        height: 70,
+    }
+  ];
+
+  export const Full_stack = [
+    {
+      skill_name: "AWS",
+      Image: "/aws.png",
+      width: 70,
+      height: 70,
+    },
+    {
+      //github
+      skill_name: "Github",
+      Image: "/github-g.gif",
+      width: 70,
+      height: 70,
+    },
+    {
+      skill_name: "Docker",
+      Image: "/docker.png",
+      width: 70,
+      height: 70,
+    },
+
+    {
+      //streamlit
+        skill_name: "Streamlit",
+        Image: "/streamlit.svg",
+        width: 70,
+        height: 70,
+    },
+    {
+      skill_name: "Generative AI Development",
+      Image: "/chatgpt-w.png",
+      width: 70,
+      height: 70,
+    },
+  ];
+
+
+
+  export const Other_skill = [
+
+  ];
+
+
+
+
