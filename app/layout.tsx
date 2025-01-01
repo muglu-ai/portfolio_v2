83ed26e3f8dda367c375b82249f0a0eed5330041
muglu <manishk_sharma@outlook.com>
2025-01-01 10:57:55 +0530

    project name update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/main/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Space Portfolio",
+  title: "Manish's Portfolio",
   description: "This is my portfolio",
 };
 
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -53,6 +53,7 @@ const HeroContent = () => {
         <motion.a
           variants={slideInFromLeft(1)}
           className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+
         >
           Learn More!
         </motion.a>
