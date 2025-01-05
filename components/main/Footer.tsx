ef4c630d12ec3c25bb45bb3db4d269b4ffdef626
muglu <manishk_sharma@outlook.com>
2025-01-05 18:31:06 +0530

    footer updated

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -33,39 +33,39 @@ const Footer = () => {
                 </div>
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                     <div className="font-bold text-[16px]">Social Media</div>
+                    {/*<p className="flex flex-row items-center my-[15px] cursor-pointer">*/}
+                    {/*    <FaYoutube />*/}
+                    {/*    <span className="text-[15px] ml-[6px]">Instagram</span>    */}
+                    {/*</p>*/}
+                    {/*<p className="flex flex-row items-center my-[15px] cursor-pointer">*/}
+                    {/*    <RxGithubLogo />*/}
+                    {/*    <span className="text-[15px] ml-[6px]">Twitter</span>    */}
+                    {/*</p>*/}
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <FaYoutube />
-                        <span className="text-[15px] ml-[6px]">Instagram</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Twitter</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxDiscordLogo />
-                        <span className="text-[15px] ml-[6px]">Linkedin</span>    
+                        <RxLinkedinLogo />
+                        <span className="text-[15px] ml-[6px]">Linkedin</span>
                     </p>
                 </div>
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                     <div className="font-bold text-[16px]">About</div>
-                   <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                     
-                        <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                      
-                        <span className="text-[15px] ml-[6px]">Learning about me</span>    
-                    </p>
+                   {/*<p className="flex flex-row items-center my-[15px] cursor-pointer">*/}
+                   {/*  */}
+                   {/*     <span className="text-[15px] ml-[6px]">Become Sponsor</span>    */}
+                   {/* </p>*/}
+                   {/* <p className="flex flex-row items-center my-[15px] cursor-pointer">*/}
+                   {/*   */}
+                   {/*     <span className="text-[15px] ml-[6px]">Learning about me</span>    */}
+                   {/* </p>*/}
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                   
-                        <span className="text-[15px] ml-[6px]">mifwebchain@gmail.com</span>    
+                        <a href="mailto:manishk_sharma@outlook.com"> <span className="text-[15px] ml-[6px]">manishk_sharma@outlook.com</span></a>
                     </p>
                 </div>
             </div>
 
-            <div className="mb-[20px] text-[15px] text-center">
-                &copy; WebChain Dev 2023 Inc. All rights reserved
-            </div>
+            {/*<div className="mb-[20px] text-[15px] text-center">*/}
+            {/*    &copy; WebChain Dev 2023 Inc. All rights reserved*/}
+            {/*</div>*/}
         </div>
     </div>
   )
